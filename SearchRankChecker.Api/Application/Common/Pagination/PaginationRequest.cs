c6a6e22fab094ecd5939e007cd55b71d70a70560
Han Chee Tan <hanchee@codecultivation.com>
2024-12-01 21:06:25 +0000

    Removed unused comments

--- SearchRankChecker.Api/Application/Common/Pagination/PaginationRequest.cs
+++ SearchRankChecker.Api/Application/Common/Pagination/PaginationRequest.cs
@@ -6,10 +6,8 @@ public abstract class PaginationRequest
 {
     public int PageIndex { get; set; }
     public int PageSize { get; set; }
-    //public string Keyword { get; set; }
     public IDictionary<string, SortingOrder> Order { get; set; }
     public IDictionary<string, string> ColumnSearch { get; set; }
-    //public ICollection<string> KeywordColumns { get; set; }
 }
 
 public enum SortingOrder
